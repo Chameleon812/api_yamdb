65c3c3c125691ae7bf143bded02b8c62eeb52b0d
Dmitry Sinkevich <Chameleon1312@yandex.com>
2022-12-29 13:57:38 +0300

    some_serial_views

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import Review, Comment
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+       
+    class Meta:
+        fields = '__all__'
+        model = Review
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    review = serializers.SlugRelatedField(
+        slug_field='text',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
