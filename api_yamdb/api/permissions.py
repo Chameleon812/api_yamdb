59044069a643581a61fc73aba69d588f636f7e31
Oleg Arsenov <arsenov_oleg@mail.ru>
2023-01-18 17:51:49 +0300

    Add some permissions and fix warnings

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -6,8 +6,8 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        #return (request.user.is_authenticated and request.user.role == 'admin')
-        return True
+        return (request.user.is_authenticated
+                and request.user.role == 'admin')
 
 
 class IsAdmin(permissions.BasePermission):
